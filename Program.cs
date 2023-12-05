1718177533394e6f05aee453bcdcb8c4f16e18b2
EugeneSusaev <SusaevEugene@gmail.com>
2023-12-05 21:58:36 +0100

    TaskCompletionSource examples were added.

--- Program.cs
+++ Program.cs
@@ -19,8 +19,34 @@ try
     Console.WriteLine(page2.ContentType);
 
     string page3 = await webPage.GetAsync(url);
+    Console.WriteLine(page3.Take(10));
+    
+    
+    Task<WebResponse> task = webPage.Get(url);
+    task.ContinueWith(task =>
+    {
+        if (task.IsFaulted)
+        {
+            Console.WriteLine($"Exception: {task.Exception?.Flatten().InnerException?.Message}");
+        }
+        else
+        {
+            Console.WriteLine(task.Result.ContentType);
+        }
+    });
 
-    Console.WriteLine(page3);
+    
+    Task<WebResponse> webRequestTask = webPage.Get(url);
+    webRequestTask.Wait();
+    
+    if (webRequestTask.IsCompletedSuccessfully)
+    {
+        Console.WriteLine(webRequestTask.Result);
+    }
+    else if (webRequestTask.IsFaulted)
+    {
+        Console.WriteLine($"Exception: {webRequestTask.Exception?.Flatten().InnerException?.Message}");
+    }
 
 }
 catch (WebException webEx)
