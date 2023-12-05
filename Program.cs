6581147fcc70ed06de1b301ad9a65c2335095d1b
EugeneSusaev <SusaevEugene@gmail.com>
2023-12-05 21:41:44 +0100

    WebPage was added to explore async/await.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using AsyncAwait.TaskIdea;
+
+string url = "https://ya.ru";
+
+try
+{
+    WebPage webPage = new WebPage();
+    
+    WebResponse content = await webPage.GetContent(url);
+    Console.WriteLine(content);
+    
+    
+    WebResponse page = await webPage.Get(url);
+    Console.WriteLine(page);
+    
+    
+    WebResponse page2 = await webPage.Get(url);
+    Console.WriteLine(page2.ContentType);
+
+    string page3 = await webPage.GetAsync(url);
+
+    Console.WriteLine(page3);
+
+}
+catch (WebException webEx)
+{
+    // Handle web exception
+    Console.WriteLine($"WebException: {webEx.Message}");
+}
+catch (Exception ex)
+{
+    // Handle other exceptions
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+
+
---
+++ TaskIdea/WebPage.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+
+namespace AsyncAwait.TaskIdea;
+
+public class WebPage
+{
+    public WebPage()
+    {
+        
+    }
+    
+    public Task<WebResponse> Get(string url)
+    {
+            WebRequest request = WebRequest.Create(url);
+            return request.GetResponseAsync();
+    }
+    
+    public async Task<WebResponse> GetContent(string url)
+    {
+        WebRequest request = WebRequest.Create(url);
+        return await request.GetResponseAsync();
+    }
+
+    public Task<string> GetAsync(string url)
+    {
+        // Create a TaskCompletionSource to represent the asynchronous operation
+        var tcs = new TaskCompletionSource<string>();
+
+        // Create a WebRequest object
+        WebRequest request = WebRequest.Create(url);
+
+        // Start the asynchronous operation
+        request.BeginGetResponse(asyncResult =>
+        {
+            try
+            {
+                WebResponse response = request.EndGetResponse(asyncResult);
+                using (Stream responseStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string content = reader.ReadToEnd();
+                    tcs.SetResult(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        }, null);
+        
+        return tcs.Task;
+    }
+    
+}
